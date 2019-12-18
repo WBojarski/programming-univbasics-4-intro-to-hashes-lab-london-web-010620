fc7879041f55e7ef9e5042c06a32669a5d835730
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-18 09:40:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,42 +1,7 @@
-def new_hash
-	empty_hash = {}
-	empty_hash
-end
-
-def my_hash
-  fruit = {
-  	apple: 2,
-  	banana: 1,
-  	mango: 10
-  }
-  fruit
-end
-
-def pioneer
-	pioneer_hash = {
-		name: "Grace Hopper"
-	}
-	pioneer_hash
-end
-
-def id_generator
-	generator = {
-		id: 333
-	}
-	generator
-end
-
 def my_hash_creator(key, value)
+	key.to_sym
 	new_hash = {
 		key: value
 	}
 	new_hash
-end
-
-def read_from_hash(hash, key)
-	hash[:key]
-end
-
-def update_counting_hash(hash, key)
-  hash[:key].nil? ? hash[:key] = 1 : hash[key] += 1
 end
